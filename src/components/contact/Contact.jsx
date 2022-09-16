978db405d509a2a0ed3dd08e3f7b32346556a65a
Surajkumarrr <surajsahu13sk@gmail.com>
2022-09-17 02:25:52 +0530

    corrected the intro image and made better responsiveness

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,37 +2,41 @@ import { useState } from 'react';
 import './contact.scss'
 
 export default function Contact() {
-  const[message,setMessage] = useState(false) ;
+  const [message, setMessage] = useState(false);
 
-  const handleSubmit=(e)=>{
-    e.preventDefault() ;
-    setMessage(true) ;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
   }
 
   return (
-    
-      <div className='contact' id='contact'>
-        <div className="left">
-          <img src="assets/contact-us.png" alt="" />
-          <div className="socialIcons">
-            <a href="https://www.linkedin.com/in/suraj-kumar-sahu-1a73401ba" target="blank"><img src="assets/linkedin.png" alt="Loading..." /></a>
-            <a href="https://github.com/Suraj-kumar-sahu" target="blank"><img src="assets/github.png" alt="Loading..." /></a>
-            <a href="https://www.instagram.com/_surajkumarrr_____/" target="blank"><img src="assets/insta.png" alt="Loading..." /></a>
-            <a href="https://www.fb.com" target="blank"><img src="assets/fb.png" alt="Loading..." /></a>
+
+    <div className='contact' id='contact'>
+      <div className="left">
+
+        {/* <img src="assets/contact-us.png" alt="" /> */}
+        <video className='videoTag' autoPlay loop muted>
+          <source src='https://media.istockphoto.com/videos/contact-us-concept-video-id1366018964' type='video/mp4' />
+        </video>
+            <div className="socialIcons">
+              <a href="https://www.linkedin.com/in/suraj-kumar-sahu-1a73401ba" target="blank"><img src="assets/linkedin.png" alt="Loading..." /></a>
+              <a href="https://github.com/Suraj-kumar-sahu" target="blank"><img src="assets/github.png" alt="Loading..." /></a>
+              <a href="https://www.instagram.com/_surajkumarrr_____/" target="blank"><img src="assets/insta.png" alt="Loading..." /></a>
+              <a href="https://www.fb.com" target="blank"><img src="assets/fb.png" alt="Loading..." /></a>
+            </div>
+            <span>Made with <img src="assets/heart.png" alt="" /> by Suraj Kumar Sahu</span>
           </div>
-          <span>Made with <img src="assets/heart.png" alt="" /> by Suraj Kumar Sahu</span>
-        </div>
-        <div className="right">
-          <h2>Contact.</h2>
-          <form onSubmit={handleSubmit}>
-            <input type="text" placeholder='Email' />
-            <textarea placeholder='Message'></textarea>
-            <button type='submit'>Send</button>
-            {message && <span>Thanks, I'll reply ASAP :) </span> } 
-          </form>
-        </div>
+          <div className="right">
+            <h2>Contact.</h2>
+            <form onSubmit={handleSubmit}>
+              <input type="text" placeholder='Email' />
+              <textarea placeholder='Message'></textarea>
+              <button type='submit'>Send</button>
+              {message && <span>Thanks, I'll reply ASAP :) </span>}
+            </form>
+          </div>
       </div>
 
-    
-  )
+
+      )
 }
